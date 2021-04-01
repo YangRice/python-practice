19db7e486df248ca4b63e822372116def0aeb6ff
yui.yi <yui.yi@alibaba-inc.com>
2021-04-02 00:22:03 +0800

    Add map

--- iterate_list.py
+++ iterate_list.py
@@ -22,6 +22,9 @@ def for_enumerate(input):
     for i, x in enumerate(input):
         yield x * 2
 
+def map_list(input):
+    return map(lambda x: x * 2, input)
+
 def list_comprehension(input):
     return [x * 2 for x in input]
     
@@ -37,9 +40,11 @@ x = list(range(5))
 print(f'for_loop: {for_loop(x)}')
 print(f'for_range: {for_range(x)}')
 print(f'for_enumerate: {for_enumerate(x)}')
+print(f'map: {map_list(x)}')
 print(f'list(for_loop): {list(for_loop(x))}')
 print(f'list(for_range): {list(for_range(x))}')
 print(f'list(for_enumerate): {list(for_enumerate(x))}')
+print(f'list(map): {list(map_list(x))}')
 print(f'list_comprehension: {list_comprehension(x)}')
 print(f'range_list_comprehension: {range_list_comprehension(x)}')
 print(f'enumerate_list_comprehension: {list_enumerate_comprehension(x)}')
@@ -51,9 +56,11 @@ x = list(range(100))
 print(f'for_loop: {measure_time(for_loop, x)}')
 print(f'for_range: {measure_time(for_range, x)}')
 print(f'for_enumerate: {measure_time(for_enumerate, x)}')
+print(f'map: {measure_time(map_list, x)}')
 print(f'list(for_loop): {measure_time(lambda x: list(for_loop(x)), x)}')
 print(f'list(for_range): {measure_time(lambda x: list(for_range(x)), x)}')
 print(f'list(for_enumerate): {measure_time(lambda x: list(for_enumerate(x)), x)}')
+print(f'list(map): {measure_time(lambda x: list(map_list(x)), x)}')
 print(f'list_comprehension: {measure_time(list_comprehension, x)}')
 print(f'range_list_comprehension: {measure_time(range_list_comprehension, x)}')
 print(f'enumerate_list_comprehension: {measure_time(list_enumerate_comprehension, x)}')
